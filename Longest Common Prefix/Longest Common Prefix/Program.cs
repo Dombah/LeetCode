3ed93b097732c7f168b39233d10cfdae5402ec56
Dombah <53014702+Dombah@users.noreply.github.com>
2023-01-06 17:03:08 +0100

    Added solution

---
+++ Longest Common Prefix/Longest Common Prefix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] strs = { "euro" , "europa", "eurici", "eurelova", "abera"};
+        Console.WriteLine(LongestCommonPrefix(strs));
+    }
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        string prefix = "";
+        string letters;
+        int currentCheckIndex = 0;
+        do
+        {
+            letters = "";
+            foreach (string s in strs)
+            {
+                letters += s[currentCheckIndex];
+            }
+            var unique = new HashSet<char>(letters);
+            if (unique.Count != 1)
+            {
+                break;
+            }
+            foreach(char c in unique)
+            {
+                prefix += c;
+            }
+            currentCheckIndex++;
+        }while (true);
+        return prefix;
+    }
+}
